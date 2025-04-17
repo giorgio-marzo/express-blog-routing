1368c0a7431dd6f9e48b5174945bba87a06a0c51
giorgio.marzo <gio.marzo96@gmail.com>
2025-04-17 17:10:19 +0200

    [UPD] rotta show

--- routers/post.js
+++ routers/post.js
@@ -11,3 +11,8 @@ const router = express.Router();
 router.get('/', (req, res) =>{
     res.send('indice dei post')
 })
+
+// operazione show
+router.get('/:id', (req, res) =>{
+    res.send(`post con id: ${req.params.id} `)
+})
