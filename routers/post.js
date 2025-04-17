81ead2eefba5ca9a96b890c9e80e43057853c047
giorgio.marzo <gio.marzo96@gmail.com>
2025-04-17 16:59:10 +0200

    [UPD] definita rotta index

--- routers/post.js
+++ routers/post.js
@@ -3,4 +3,11 @@ const express = require('express');
 
 // inizializziamo router
 
-const router = express.Router()
+const router = express.Router();
+
+// difiniamo le rotte delle operazioni di post attraverso i metodi get, post, put ec...
+
+// rotta index
+router.get('/', (req, res) =>{
+    res.send('indice dei post')
+})
