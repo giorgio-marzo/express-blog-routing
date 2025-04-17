71e7a26894dc5237fd59b2790044e1defe7e861e
giorgio.marzo <gio.marzo96@gmail.com>
2025-04-17 15:57:33 +0200

    [UPD] impostato porta server e rotta base di app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log(`prova`)
+//console.log(`prova`)
+
+// import express
+const express = require('express');
+
+// inizializziamo express
+
+const app = express()
+
+// definisco porta
+
+const port = 3000;
+
+// diciamo al server di rimanere in ascolto su la suddetta porta
+
+app.listen(port, () => {
+    console.log(`server in ascolto nella porta ${port}`)
+})
+
+//inserisco rotta base
+
+app.get('/', (req, res) =>{
+    res.send('Server del mio blog modificabile')
+})
